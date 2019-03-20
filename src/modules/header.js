e1d908fa5bd2321393613167857154a8b04dc4b8
tikken <tikken23@gmail.com>
2019-03-20 13:32:06 +0300

    UglifyJSPluginDoesntWorkInES6

--- src/modules/header.js
+++ src/modules/header.js
@@ -1,12 +1,4 @@
 import $ from 'jquery'
 
-$('<h1/>')
-  .text('Hello motherfucka')
-  .css({
-      textAlign: 'center',
-      color: 'red'
-  })
-  .appendTo($('header'))
-
 console.log('sum')
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 const Html = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -7,18 +10,27 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin({}),
+            new UglifyJsPlugin({})
+        ]
+    },
 
     plugins: [
         new Html({
             filename: 'index.html',
             template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
         })
     ],
     module: {
         rules: [
             {
             test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+            use: [MiniCssExtractPlugin.loader, 'css-loader']
          }
       ]
     }
